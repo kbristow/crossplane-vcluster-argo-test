04ff0e8a84c532b535be21886b8d3ffcb44fa3d4
Kieran Bristow <kieran@bristow.za.net>
2023-07-07 17:00:01 +0200

    first commit

---
+++ scripts/init-k3d-demo-env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Run from the directory this script lives in
+MYDIR=$(dirname $0)
+k3d cluster delete cf-kubecon23-demo
+k3d cluster create cf-kubecon23-demo --volume $(pwd)/certs/ZscalerRootCA.crt:/etc/ssl/certs/ZscalerRootCA.crt
+k3d kubeconfig get cf-kubecon23-demo > /tmp/k3d-cf-kubecon23-demo.config
+export KUBECONFIG=/tmp/k3d-cf-kubecon23-demo.config
+helm repo add argo https://argoproj.github.io/argo-helm && helm repo update
+# https://github.com/cf-kubecon23-demo/cf-kubecon23-demo/issues/2121
+helm install --repo https://argoproj.github.io/argo-helm --create-namespace --namespace argocd argocd argo-cd --version 5.37.1  --set "configs.cm.application\.resourceTrackingMethod=annotation" --wait
+kubectl -n argocd apply -f $MYDIR/../argocd-applications
