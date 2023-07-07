354de3fc3dce3fd0b7e1298032acc2ae65bbdd10
Kieran Bristow <kieran@bristow.za.net>
2023-07-07 21:15:46 +0200

    Use app of apps and sync waves to apply kyverno first

--- scripts/init-k3d-demo-env.sh
+++ scripts/init-k3d-demo-env.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Run from the directory this script lives in
-MYDIR=$(dirname $0)
 k3d cluster delete cf-kubecon23-demo
 k3d cluster create cf-kubecon23-demo --volume $(pwd)/certs/ZscalerRootCA.crt:/etc/ssl/certs/ZscalerRootCA.crt
 k3d kubeconfig get cf-kubecon23-demo > /tmp/k3d-cf-kubecon23-demo.config
@@ -8,4 +7,4 @@ export KUBECONFIG=/tmp/k3d-cf-kubecon23-demo.config
 helm repo add argo https://argoproj.github.io/argo-helm && helm repo update
 # https://github.com/cf-kubecon23-demo/cf-kubecon23-demo/issues/2121
 helm install --repo https://argoproj.github.io/argo-helm --create-namespace --namespace argocd argocd argo-cd --version 5.37.1  --values argocd-values.yaml --set "configs.cm.application\.resourceTrackingMethod=annotation" --wait
-kubectl -n argocd apply -f $MYDIR/../argocd-applications
+kubectl -n argocd apply -f app-of-apps.yaml
