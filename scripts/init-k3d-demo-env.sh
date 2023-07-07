c9c6ce625e80bc1b88c8b8021b41e18ff6aeec40
Kieran Bristow <kieran@bristow.za.net>
2023-07-07 21:10:25 +0200

    Add zscaler cert for kyverno, use serverside apply for kyverno

--- scripts/init-k3d-demo-env.sh
+++ scripts/init-k3d-demo-env.sh
@@ -7,5 +7,5 @@ k3d kubeconfig get cf-kubecon23-demo > /tmp/k3d-cf-kubecon23-demo.config
 export KUBECONFIG=/tmp/k3d-cf-kubecon23-demo.config
 helm repo add argo https://argoproj.github.io/argo-helm && helm repo update
 # https://github.com/cf-kubecon23-demo/cf-kubecon23-demo/issues/2121
-helm install --repo https://argoproj.github.io/argo-helm --create-namespace --namespace argocd argocd argo-cd --version 5.37.1  --set "configs.cm.application\.resourceTrackingMethod=annotation" --wait
+helm install --repo https://argoproj.github.io/argo-helm --create-namespace --namespace argocd argocd argo-cd --version 5.37.1  --values argocd-values.yaml --set "configs.cm.application\.resourceTrackingMethod=annotation" --wait
 kubectl -n argocd apply -f $MYDIR/../argocd-applications
