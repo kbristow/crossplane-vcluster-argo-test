0787eeb5bc169ba219c43959d443d399700d92ad
Kieran Bristow (ZA) <kieran.bristow@absa.africa>
2023-07-14 10:39:13 +0200

    Fixed cert issue

--- scripts/init-k3d-demo-env.sh
+++ scripts/init-k3d-demo-env.sh
@@ -7,4 +7,5 @@ export KUBECONFIG=/tmp/k3d-cf-kubecon23-demo.config
 helm repo add argo https://argoproj.github.io/argo-helm && helm repo update
 # https://github.com/cf-kubecon23-demo/cf-kubecon23-demo/issues/2121
 helm install --repo https://argoproj.github.io/argo-helm --create-namespace --namespace argocd argocd argo-cd --version 5.37.1  --values argocd-values.yaml --set "configs.cm.application\.resourceTrackingMethod=annotation" --wait
+kubectl apply -f argo-helm-repo.yaml
 kubectl -n argocd apply -f app-of-apps.yaml
